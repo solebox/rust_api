30b08ed0447db8e0b214d2ad87efff17b447bb11
koby <koby.kilimnik@imperva.com>
2017-04-18 19:00:59 +0300

    woot woot, just insert to db and we are golden

--- src/main.rs
+++ src/main.rs
@@ -14,25 +14,35 @@ struct TestStruct {
     pass: String
 }
 
+#[derive(RustcDecodable, RustcEncodable)]
+struct DecMessage {
+    service: String
+}
 
 fn main() {
-
+    //test_enc();
 
     Server::http("0.0.0.0:8001").unwrap().handle(|mut req: Request, mut res: Response| {
         let key = [149, 251, 204, 100, 110, 129, 252, 206, 71, 66, 193, 99, 43, 218, 49, 35, 199, 112, 22, 154, 126, 9, 226, 228, 49, 162, 243, 50, 1, 174, 207, 254];
         match req.method {
             hyper::Post => {
                 let mut result = "".to_string();
+
+                let my_remote = format!("{}\n", req.remote_addr);
+                //result.push_str(&my_remote);
+
+                let my_version = format!("{}\n", req.version);
+                result.push_str(&my_version);
+
+
                 for header in req.headers.iter() {
                     let my_headers = format!("{}:{}\n", 
                                         header.name(), 
                                         &header.value_string().to_string());
                     result.push_str(&my_headers);
                 }
-                let my_version = format!("{}\n", req.version);
-                result.push_str(&my_version);
-                let my_remote = format!("{}\n", req.remote_addr);
-                result.push_str(&my_remote);
+
+
                 
                 // body handling
                 let mut my_body = String::new();
@@ -42,14 +52,16 @@ fn main() {
                 result.push_str(&my_body);
                 let my_decoded_body = format!("\ntoken:{}\n", decoded_body.token);
 
-                result.push_str(&my_decoded_body);
+//                result.push_str(&my_decoded_body);
 
                 let decrypted = dec(&decoded_body.token, &key);
                 let my_decrypted_token = format!("decrypted: {}", decrypted);
+                //result.push_str(&my_decrypted_token);
 
-                result.push_str(&my_decrypted_token);
+                insert_to_db(&decrypted, &req.remote_addr, &result);
 
-                write!(&mut res.start().unwrap(),"{}",&result).unwrap();
+                let response = "wrong creds";
+                write!(&mut res.start().unwrap(),"{}",&response).unwrap();
                 //io::copy(&mut req, &mut res.start().unwrap()).unwrap();
             },
             _ => *res.status_mut() = StatusCode::MethodNotAllowed
@@ -57,9 +69,16 @@ fn main() {
     }).unwrap();
 }
 
+fn insert_to_db(decrypted_token: &String, remote_ip: &std::net::SocketAddr, request_data: &String) {
+    //implement insertion to mongodb
+    let decrypted_json_msg: DecMessage = json::decode(decrypted_token).unwrap();
+    println!("service: {}", decrypted_json_msg.service);
+    println!("remote_ip: {}", remote_ip);
+    println!("ver_headers_body: {}", request_data);
+}
 
 fn test_enc(){
-    let message = "Hello World!";
+    let message = "{\"service\": \"storm proxies\"}";
 
    // let mut key: [u8; 32] = [0; 32];
 
