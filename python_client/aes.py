752976fe3912b5a5133ad7035ec1827bda0aba28
solebox <5013box@gmail.com>
2017-04-18 23:17:50 +0300

    changed key to fit client, will change again chillax ;)

---
+++ python_client/aes.py
@@ -0,0 +1,36 @@
+import base64
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import AES
+
+class AESCipher(object):
+
+    def __init__(self, key): 
+        self.bs = 16 
+        self.key = hashlib.sha256(key.encode()).digest()
+        res = [k for k in self.key]
+        #print(res)
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(cipher.encrypt(raw) + iv)
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[len(enc)-AES.block_size:]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[:len(enc)-AES.block_size])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
+
+if __name__ == "__main__":
+    aes = AESCipher("klutsh")
+    encrypted = aes.encrypt('{"service":"ballz sackz"}')
+    decrypted = aes.decrypt(encrypted)
+    print(decrypted)
